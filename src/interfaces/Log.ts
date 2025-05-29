65b2f63aecdc436cc5021fb07bb7c0fffb81c924
Ryan McCarthy <ryan.mccarthy@ithaka.org>
2025-05-29 07:06:34 -0700

    Update Log interface

--- src/interfaces/Log.ts
+++ src/interfaces/Log.ts
@@ -3,4 +3,12 @@ export interface Log {
   event_description: string
   itemid?: string
   frontend_error?: string
+  comments?: string
+  reason?: string
+  dois?: string[]
+  groups?: number[]
+  journals?: string[]
+  disciplines?: string[]
+  code?: string
+  page_index?: number
 }
