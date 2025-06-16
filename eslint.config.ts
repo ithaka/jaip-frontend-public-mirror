bba4880b7dade2136721fd18ae25658ac69c1053
Ryan McCarthy <ryan.mccarthy@ithaka.org>
2025-06-16 08:46:12 -0700

    Update eslint config

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,35 +1,40 @@
-import pluginVue from 'eslint-plugin-vue'
-import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
- 
-import pluginCypress from 'eslint-plugin-cypress/flat'
-import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+import ithakaBaseEslintConfig from "@ithaka/eslint-config-base";
+import ithakaVueEslintConfig from "@ithaka/eslint-config-vue";
+import { defineConfig, globalIgnores } from "eslint/config";
 
-// To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
-// import { configureVueProject } from '@vue/eslint-config-typescript'
-// configureVueProject({ scriptLangs: ['ts', 'tsx'] })
-// More info at https://github.com/vuejs/eslint-config-typescript/#advanced-setup
+export default [
+  ...ithakaBaseEslintConfig,
+  ...ithakaVueEslintConfig,
 
-export default defineConfigWithVueTs(
-  {
-    name: 'app/files-to-lint',
-    files: ['**/*.{ts,mts,tsx,vue}'],
-  },
-
-  {
-    name: 'app/files-to-ignore',
-    ignores: ['**/PageViewer.vue', '**/dist/**', '**/dist-ssr/**', '**/coverage/**', '**/.yarn/**', '**/public/scripts/**'],
-  },
-
-  pluginVue.configs['flat/essential'],
-  vueTsConfigs.recommended,
+  ...defineConfig([globalIgnores([
+    "dist/**",
+    "node_modules/**",
+    ".yarn/**",
+    "public/**",
+  ])]),
   
   {
-    ...pluginCypress.configs.recommended,
-    files: [
-      '**/__tests__/*.{cy,spec}.{js,ts,jsx,tsx}',
-      'cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}',
-      'cypress/support/**/*.{js,ts,jsx,tsx}'
-    ],
+    languageOptions: {
+      globals: {
+        window: true,
+        document: true,
+        navigator: true,
+        URL: true,
+        setTimeout: true,
+        clearTimeout: true,
+        setInterval: true,
+        clearInterval: true,
+        HTMLDivElement: true,
+        HTMLElement: true,
+        HTMLInputElement: true,
+        location: true,
+        console: true,
+        KeyboardEvent: true,
+        URLSearchParams: true,
+        module: true,
+
+      },
+    },
   },
-  skipFormatting,
-)
+
+];
