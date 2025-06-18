615c9160f91799ae8b93a3183024a8b09fd82b0b
Ryan McCarthy <ryan.mccarthy@ithaka.org>
2025-06-18 12:39:50 -0700

    Add headid to tooltip id

--- src/components/truncated/CheckboxGroup.vue
+++ src/components/truncated/CheckboxGroup.vue
@@ -123,7 +123,7 @@ const iconKey = ref(displayList.value.map(() => 0))
                   <BulkApprovalIcon
                     v-if="item.bulk_approval && getBulkApprovalStatus(item.bulk_approval, groupIDs)"
                     :key="iconKey[i]"
-                    :tooltip-id="`approval_${item.code}`"
+                    :tooltip-id="`approval_${item.code || item.headid}`"
                     :text="`Articles in ${getLabel(item)} are generally approved automatically.`"
                     :disc="item"
                     @render="iconKey[i]++"
